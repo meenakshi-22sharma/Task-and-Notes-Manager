4b48a5cf52ed68f5db59912ba6c1b6d32fb68edd
Meenakshi Sharma <mee2208sharma@gmail.com>
2020-04-14 18:47:51 +0530

    add controller

---
+++ controller/taskController.js
@@ -0,0 +1,12 @@
+function listAll(req, res){ 
+  
+    // The render method takes the name of the HTML 
+    // page to be rendered as input 
+    // This page should be in the views folder 
+    // in the root directory. 
+    res.render('home'); 
+      
+    }; 
+function addNew(){}
+function details(){}
+function updateDetails(){}
