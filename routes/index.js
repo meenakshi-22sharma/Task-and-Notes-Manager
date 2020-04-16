6ccabf77748cf9e8754e46e6df24c14593c6ef33
Meenakshi Sharma <mee2208sharma@gmail.com>
2020-04-16 20:28:12 +0530

    route configure

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,24 @@
-const express= require('express').Router()
-
-
-const route = express()
+const router= require('express').Router()
 const taskController = require('../controller/taskController')
 const notesController = require('../controller/notesController')
 
   
 //list all tasks in array
-route.get('/tasks',taskController.listAll)
+router.get('/tasks',taskController.listAll)
 //add a new todo
-route.post('/tasks',taskController.addNew)
+router.post('/tasks',taskController.addNew)
 //details of todo with requested id 
-route.get('/tasks/:id',taskController.details)
+router.get('/tasks/:id',taskController.details)
 //update details of todo with requested id
-route.patch('/tasks/:id', taskController.updateDetails)
+router.patch('/tasks/:id', taskController.updateDetails)
 //get list of all notes under todo with requested id
-route.get('/tasks/:id/notes', notesController.getAll)
+router.get('/tasks/:id/notes', notesController.getAll)
 //add a new note under todo with given id 
-route.post('/tasks/:id/notes', notesController.addNewNote)
+router.post('/tasks/:id/notes', notesController.addNewNote)
 
 
 
 
 exports =module.exports = {
-  route
+  router
 }
