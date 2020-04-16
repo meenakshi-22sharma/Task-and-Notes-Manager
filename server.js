184b0e744df191e526287aa5776916307dd19c10
Meenakshi Sharma <mee2208sharma@gmail.com>
2020-04-16 20:31:03 +0530

    app origin

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path =require('path')
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use('/',express.static(path.join(__dirname, 'views')))
-app.use('/api',require('./routes').route)
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/api',express.static(path.join(__dirname, 'views/home.html')))
+app.use(require('./routes').router)
 app.listen(4567,() => console.log('Server started at http://localhost:4567'))
-
 
