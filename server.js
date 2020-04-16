a4f7befe168b1644c9a03b13b86e4574672d7811
Meenakshi Sharma <mee2208sharma@gmail.com>
2020-04-17 00:08:39 +0530

    redirect to landing page

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => res.redirect('/api'))
 app.use('/api',express.static(path.join(__dirname, 'views/home.html')))
 app.use(require('./routes').router)
 //app.listen(4567,() => console.log('Server started at http://localhost:4567'))
 app.listen(process.env.PORT || 4567)
+
