283087310424e298cedbd5cdd2cf6877619c5420
Meenakshi Sharma <mee2208sharma@gmail.com>
2020-04-16 23:54:12 +0530

    process env port add

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api',express.static(path.join(__dirname, 'views/home.html')))
 app.use(require('./routes').router)
-app.listen(4567,() => console.log('Server started at http://localhost:4567'))
-
+//app.listen(4567,() => console.log('Server started at http://localhost:4567'))
+app.listen(process.env.PORT || 4567)
